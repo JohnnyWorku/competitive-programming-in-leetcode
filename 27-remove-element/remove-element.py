c8d443c3271d40d98456896b965f0c78241b34c6
JohnnyWorku <johnnythegold41@gmail.com>
2024-11-26 01:52:06 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (28.66%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,15 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0  # Initialize the index counter
-    
-        while k < len(nums):
-            if nums[k] == val:
-                nums.pop(k)  # Remove the element if it matches `val`
-            else:
-                k += 1  # Only increment `k` if we don't remove an element
-        
-        return len(nums)
+        k = 0
 
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k], nums[i] = nums[i], nums[k]
+                k += 1
 
-
-
+        return k
