88f16318bff64ce68e6dde3fb5b079d1c12022cb
JohnnyWorku <johnnythegold41@gmail.com>
2024-12-13 01:05:01 +0300

    Time: 7 ms (59.02%) | Memory: 18.2 MB (17.77%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -13,4 +13,29 @@ class Solution:
 
         return -1
 
+
+
+
+
+
+
+
+
+
+
+
+
+        # total = sum(nums)
+        # left_sum = 0
+
+        # for i in range(len(nums)):
+        #     right_sum = total - left_sum - nums[i]
+
+        #     if left_sum == right_sum:
+        #         return i
+
+        #     left_sum += nums[i]
+
+        # return -1
+
        
