facad7a267d3962a5090aeb7e36f448ccd6c1b27
JohnnyWorku <johnnythegold41@gmail.com>
2024-12-17 10:08:03 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (6.20%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -2,28 +2,17 @@ class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
 
-        def is_num(character: str):
-            if character.startswith('-'):
-                return character[1:].isdigit()
-            return character.isdigit()
+        for char in operations:
+            if char == "+":
+                stack.append(stack[-1] + stack[-2])
 
-        for operation in operations:
-            if is_num(operation):
-                stack.append(int(operation))
-
-            elif len(stack) >= 2 and operation == '+':
-                summation = stack[-1] + stack[-2]
-                stack.append(summation)
-
-            elif stack and operation == 'C':
+            elif char == "C":
                 stack.pop()
 
+            elif char == "D":
+                stack.append(stack[-1] * 2)
+
             else:
-                product = stack[-1] * 2
-                stack.append(product)
+                stack.append(int(char))
 
-        output = 0
-        for i in stack:
-            output += i
-
-        return output
+        return sum(stack)
