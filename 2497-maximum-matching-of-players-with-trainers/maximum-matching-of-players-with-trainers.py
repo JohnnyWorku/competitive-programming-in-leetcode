1fec7769f79412d6cf52896d132ecc48c691d52e
JohnnyWorku <johnnythegold41@gmail.com>
2024-12-17 11:03:02 +0300

    Time: 75 ms (61.80%) | Memory: 34.1 MB (5.83%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.py
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.py
@@ -6,11 +6,11 @@ class Solution:
 
         i = 0
         j = 0
-        
+
         while i < len(players) and j < len(trainers):
             if players[i] <= trainers[j]:
                 count += 1
                 i += 1
-            j += 1 
+            j += 1
 
         return count
