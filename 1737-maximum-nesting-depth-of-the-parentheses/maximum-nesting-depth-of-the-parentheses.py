f97ca9ca94174a6e1c8f1feae72431d5fc779061
JohnnyWorku <johnnythegold41@gmail.com>
2024-12-24 04:02:14 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (6.93%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
         bracket_dict = {"(": ")"}
-        stack = []
         count = 0
         max_count = 0
 
         for char in s:
             if char == "(":
-                stack.append(char)
                 count += 1
 
             elif char == ")":
                 max_count = max(max_count, count)
-                stack.pop()
                 count -= 1
 
         return max_count
