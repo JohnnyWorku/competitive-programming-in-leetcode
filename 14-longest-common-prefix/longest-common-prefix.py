ba7f3609d61eb8b801318d0cd6e4f1fde9f92a26
JohnnyWorku <johnnythegold41@gmail.com>
2025-02-02 01:48:11 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (63.06%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,33 +1,33 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # pref = strs[0]
-        # pref_len = len(pref)
+        pref = strs[0]
+        pref_len = len(pref)
 
-        # for s in strs[1 : ]:
-        #     while pref != s[0 : pref_len]:
-        #         pref_len -= 1
-        #         pref = pref[0: pref_len]
+        for s in strs[1 : ]:
+            while pref != s[0 : pref_len]:
+                pref_len -= 1
+                pref = pref[0: pref_len]
                 
-        #         if pref_len == 0:
-        #             return ""
+                if pref_len == 0:
+                    return ""
 
-        # return pref[0 : pref_len]
-        count = 0
-        strs = sorted(strs, key=len)
-        # print(strs)
-        first_str = strs[0]
-        common_prefix = ""
+        return pref[0 : pref_len]
+        # count = 0
+        # strs = sorted(strs, key=len)
+        # # print(strs)
+        # first_str = strs[0]
+        # common_prefix = ""
 
-        for i in range(len(first_str)):
-            for word in strs:
-                if first_str[i] == word[i]:
-                    count += 1
+        # for i in range(len(first_str)):
+        #     for word in strs:
+        #         if first_str[i] == word[i]:
+        #             count += 1
 
-            if count == len(strs):
-                common_prefix += first_str[i]
-                count = 0
+        #     if count == len(strs):
+        #         common_prefix += first_str[i]
+        #         count = 0
 
-            else:
-                return common_prefix
+        #     else:
+        #         return common_prefix
 
-        return common_prefix
+        # return common_prefix
