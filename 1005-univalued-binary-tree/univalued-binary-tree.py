b968adb5ba734f6324fa3c5f74602f08e41b5089
Yohannes Worku <johnnythegold41@gmail.com>
2025-06-13 21:14:47 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.52%) - LeetSync

--- 1005-univalued-binary-tree/univalued-binary-tree.py
+++ 1005-univalued-binary-tree/univalued-binary-tree.py
@@ -6,23 +6,41 @@
 #         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        # BFS
-        queue = deque([root])
-        is_univalued = True
-        init_value = root.val
+        # # BFS
+        # queue = deque([root])
+        # is_univalued = True
+        # init_value = root.val
 
-        while queue:
-            node = queue.popleft()
+        # while queue:
+        #     node = queue.popleft()
+
+        #     if node.val != init_value:
+        #         is_univalued = False
+        #         break
+
+        #     if node.left:
+        #         queue.append(node.left)
+        #     if node.right:
+        #         queue.append(node.right)
+
+        # return is_univalued
+
+        # DFS
+        init_value = root.val
+        def dfs(node):
+            nonlocal init_value
 
             if node.val != init_value:
-                is_univalued = False
-                break
+                return False
 
             if node.left:
-                queue.append(node.left)
+                if not dfs(node.left):
+                    return False
             if node.right:
-                queue.append(node.right)
+                if not dfs(node.right):
+                    return False
 
-        return is_univalued
+            return True
 
+        return dfs(root)
         
